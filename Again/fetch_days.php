8aff5845b094e7ecb24f2e94d842b8311b0798c1
Nasir <nasir.rashid564@gmail.com>
2025-01-24 09:39:33 +0000

    Removed Again folder

--- Again/fetch_days.php
+++
@@ -1,9 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;dbname=activity_booking', 'root', '');
-
-$query = "SELECT DISTINCT day FROM slots WHERE day >= CURDATE() ORDER BY day ASC";
-$stmt = $pdo->query($query);
-$days = $stmt->fetchAll(PDO::FETCH_COLUMN);
-
-echo json_encode($days);
-?>
